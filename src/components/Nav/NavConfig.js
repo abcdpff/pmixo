5a0bf4dc4a525980e64c8d7069e9666575873996
April <april@AprildeMacBook-Pro.local>
2022-02-23 12:31:25 +0800

    Logo and navLink update

--- src/components/Nav/NavConfig.js
+++ src/components/Nav/NavConfig.js
@@ -1,108 +1,193 @@
 export const navData = {
   navData: [
     {
-      label: '家居',
-      id: '家居',
+      label: 'Home Furniture',
+      id: 'home-furniture',
       categories: [
         {
-          label: '书桌/书桌椅',
+          label: 'Beds',
           url: '',
-          imageURL: 'https://706d-pmixo-7gray7k8850a1952-1308466752.tcb.qcloud.la/image/%E5%AE%B6%E5%B1%85/%E4%B9%A6%E6%A1%8C.png?sign=a3ad886010582a8206784de7017b03a4&t=1645329092'
+          imageURL: 'https://706d-pmixo1-6gno1a2f0b6f1c2d-1308466752.tcb.qcloud.la/PMIXO%20Product%20Line/Home%20Furniture/Beds.png?sign=1c10815051787ef10593632ef072609b&t=1645428887'
         },
         {
-          label: '书架/书柜',
+          label: 'Bookcases',
           url: '',
-          imageURL: 'https://706d-pmixo-7gray7k8850a1952-1308466752.tcb.qcloud.la/image/%E5%AE%B6%E5%B1%85/%E4%B9%A6%E6%9F%9C.png?sign=cfd0ec9e42f13749c710fab976fefd55&t=1645329119'
+          imageURL: 'https://706d-pmixo1-6gno1a2f0b6f1c2d-1308466752.tcb.qcloud.la/PMIXO%20Product%20Line/Home%20Furniture/Bookcases.png?sign=b6e6644046bacb1eff6d04efcee02c6b&t=1645428952'
         },
         {
-          label: '餐桌/餐桌椅',
+          label: 'Bunk Beds',
           url: '',
-          imageURL: 'https://706d-pmixo-7gray7k8850a1952-1308466752.tcb.qcloud.la/image/%E5%AE%B6%E5%B1%85/%E9%A4%90%E6%A1%8C%E6%A4%85.png?sign=74f2c569f5f5f12925716f2ec09aee39&t=1645329156'
+          imageURL: 'https://706d-pmixo1-6gno1a2f0b6f1c2d-1308466752.tcb.qcloud.la/PMIXO%20Product%20Line/Home%20Furniture/Bunk%20Beds.png?sign=6d7976c85595197abc2bb4878e1190ea&t=1645428966'
         },
         {
-          label: '沙发/茶几',
+          label: 'Chairs',
           url: '',
-          imageURL: 'https://706d-pmixo-7gray7k8850a1952-1308466752.tcb.qcloud.la/image/%E5%AE%B6%E5%B1%85/%E8%8C%B6%E5%87%A0.png?sign=0e1e3f7a0854e2d0b97a8e80b65789da&t=1645329179'
+          imageURL: 'https://706d-pmixo1-6gno1a2f0b6f1c2d-1308466752.tcb.qcloud.la/PMIXO%20Product%20Line/Home%20Furniture/Chairs.png?sign=586c248c873e4f07ee723a646f7be661&t=1645428977'
         },
         {
-          label: '电视柜',
+          label: 'Desks',
           url: '',
-          imageURL: 'https://706d-pmixo-7gray7k8850a1952-1308466752.tcb.qcloud.la/image/%E5%AE%B6%E5%B1%85/%E7%94%B5%E8%A7%86%E6%9F%9C.png?sign=8ca5742a1a4da941627d9d51d28f6e19&t=1645329207'
+          imageURL: 'https://706d-pmixo1-6gno1a2f0b6f1c2d-1308466752.tcb.qcloud.la/PMIXO%20Product%20Line/Home%20Furniture/Desks.png?sign=26364cca25a511445391a8088b0b0224&t=1645428986'
         },
         {
-          label: '床/床垫',
+          label: 'Dinning Sets',
           url: '',
-          imageURL: 'https://706d-pmixo-7gray7k8850a1952-1308466752.tcb.qcloud.la/image/%E5%AE%B6%E5%B1%85/%E5%BA%8A.png?sign=08c0e299533918997908ac9d93ed6acc&t=1645329222'
+          imageURL: 'https://706d-pmixo1-6gno1a2f0b6f1c2d-1308466752.tcb.qcloud.la/PMIXO%20Product%20Line/Home%20Furniture/Dinning%20Sets.png?sign=995fae0795f9ba748ad786cd502caaf3&t=1645428996'
+        },
+        
+        {
+          label: 'Dinning Tables',
+          url: '',
+          imageURL: 'https://706d-pmixo1-6gno1a2f0b6f1c2d-1308466752.tcb.qcloud.la/PMIXO%20Product%20Line/Home%20Furniture/Dinning%20Tables.png?sign=40ec09148c852ef60844ec731ca807f6&t=1645429004'
         },
         {
-          label: '床头柜/斗柜',
+          label: 'Mattresses',
           url: '',
-          imageURL: 'https://706d-pmixo-7gray7k8850a1952-1308466752.tcb.qcloud.la/image/%E5%AE%B6%E5%B1%85/%E5%BA%8A%E5%A4%B4%E6%9F%9C.png?sign=a00725c29e3873322928916a3762517e&t=1645329268'
+          imageURL: 'https://706d-pmixo1-6gno1a2f0b6f1c2d-1308466752.tcb.qcloud.la/PMIXO%20Product%20Line/Home%20Furniture/Mattresses.png?sign=1b7c848e8333353dce5c05a6a88d19b9&t=1645429013'
         },
         {
-          label: '衣柜',
+          label: 'Nightstands',
           url: '',
-          imageURL: 'https://706d-pmixo-7gray7k8850a1952-1308466752.tcb.qcloud.la/image/%E5%AE%B6%E5%B1%85/%E8%A1%A3%E6%9F%9C.png?sign=65a7c2cdde0c5cc20d4202d1f01903c0&t=1645329286'
+          imageURL: 'https://706d-pmixo1-6gno1a2f0b6f1c2d-1308466752.tcb.qcloud.la/PMIXO%20Product%20Line/Home%20Furniture/Nightstands.png?sign=5f5a1e8b5a1bfae94a0cdb9dc251b9c8&t=1645429021'
         },
         {
-          label: '鞋柜/屏风柜',
+          label: 'Shoe Cabinets',
           url: '',
-          imageURL: 'https://706d-pmixo-7gray7k8850a1952-1308466752.tcb.qcloud.la/image/%E5%AE%B6%E5%B1%85/%E9%9E%8B%E6%9F%9C.png?sign=88d0336d47ff862e5992e153e7f8848c&t=1645329313'
+          imageURL: 'https://706d-pmixo1-6gno1a2f0b6f1c2d-1308466752.tcb.qcloud.la/PMIXO%20Product%20Line/Home%20Furniture/Shoe%20Cabinets.png?sign=d7b7e9474a25b7f272768687c6df210d&t=1645429028'
         },
         {
-          label: '储物/收纳',
+          label: 'Sofas',
           url: '',
-          imageURL: 'https://706d-pmixo-7gray7k8850a1952-1308466752.tcb.qcloud.la/image/%E5%AE%B6%E5%B1%85/%E6%9F%9C%E5%AD%90/1.jpg?sign=933443943c209145aff9788147ff7cca&t=1645329366'
+          imageURL: 'https://706d-pmixo1-6gno1a2f0b6f1c2d-1308466752.tcb.qcloud.la/PMIXO%20Product%20Line/Home%20Furniture/Sofas.png?sign=6381b40f9db1d2ffbd1b2bbe044ab445&t=1645429035'
         },
         {
-          label: '儿童家具',
+          label: 'Storage Cubes',
+          url: '',
+          imageURL: 'https://706d-pmixo1-6gno1a2f0b6f1c2d-1308466752.tcb.qcloud.la/PMIXO%20Product%20Line/Home%20Furniture/Storage%20Cubes.jpeg?sign=d0978223160d4f797b56feddfa6abf21&t=1645429044'
+        },
+        {
+          label: 'Tea Tables',
           url: '',
           imageURL: 'https://706d-pmixo-7gray7k8850a1952-1308466752.tcb.qcloud.la/image/%E5%AE%B6%E5%B1%85/%E9%AB%98%E4%BD%8E%E5%BA%8A.png?sign=3a378ea8219b008dd2865db67afb2bf4&t=1645329401'
         },
+        {
+          label: 'TV Stands',
+          url: '',
+          imageURL: 'https://706d-pmixo1-6gno1a2f0b6f1c2d-1308466752.tcb.qcloud.la/PMIXO%20Product%20Line/Home%20Furniture/TV%20Stands.png?sign=401c884df0945d103b0f99fe0bebe549&t=1645429053'
+        },
+        {
+          label: 'Tea Tables',
+          url: '',
+          imageURL: 'https://706d-pmixo1-6gno1a2f0b6f1c2d-1308466752.tcb.qcloud.la/PMIXO%20Product%20Line/Home%20Furniture/Tea%20Tables.png?sign=7cbb8fd5b36556b828c2ccd20d50d970&t=1645429060'
+        },
+        {
+          label: 'Wardrobes',
+          url: '',
+          imageURL: 'https://706d-pmixo1-6gno1a2f0b6f1c2d-1308466752.tcb.qcloud.la/PMIXO%20Product%20Line/Home%20Furniture/Wardrobes.png?sign=c6a40950d449dd7167bdee38f78cc581&t=1645429097'
+        },
       ]
     },
     {
-      label: '办公',
-      id: '办公',
+      label: 'Commercial Furniture',
+      id: 'commercial-furniture',
       categories: [
         {
-          label: '书桌/书桌椅2',
+          label: 'Bar Tables',
           url: '',
-          imageURL: 'https://res.app.ikea.cn/static/public/subcatalog/bm003/thumbnail.png?v=20220104'
+          imageURL: 'https://706d-pmixo1-6gno1a2f0b6f1c2d-1308466752.tcb.qcloud.la/PMIXO%20Product%20Line/Commercial%20Furniture/Bar%20Tables.png?sign=e29ad06c4b0f47ac826721827d8aa3f0&t=1645431152'
+        },
+        {
+          label: 'Coffee Tables',
+          url: '',
+          imageURL: 'https://706d-pmixo1-6gno1a2f0b6f1c2d-1308466752.tcb.qcloud.la/PMIXO%20Product%20Line/Commercial%20Furniture/Coffee%20Tables.png?sign=5647cbc865880317a7c726135d86dc12&t=1645431175'
         }
       ]
     },
     {
-      label: '商用',
-      id: '商用',
+      label: 'Office Furniture',
+      id: 'office-furniture',
       categories: [
         {
-          label: '书桌/书桌椅3',
+          label: 'Bookcases',
           url: '',
-          imageURL: 'https://res.app.ikea.cn/static/public/subcatalog/bm003/thumbnail.png?v=20220104'
-        }
+          imageURL: 'https://706d-pmixo1-6gno1a2f0b6f1c2d-1308466752.tcb.qcloud.la/PMIXO%20Product%20Line/Office%20Furniture/Bookcases.png?sign=da7ea746ff037f9c878b03e52e0627ec&t=1645431266'
+        },
+        {
+          label: 'Conference Chairs',
+          url: '',
+          imageURL: 'https://706d-pmixo1-6gno1a2f0b6f1c2d-1308466752.tcb.qcloud.la/PMIXO%20Product%20Line/Office%20Furniture/Conference%20Chairs.png?sign=a346105c5468afe5ae405d9f9cbc92b7&t=1645431279'
+        },
+        {
+          label: 'Conference Tables',
+          url: '',
+          imageURL: 'https://706d-pmixo1-6gno1a2f0b6f1c2d-1308466752.tcb.qcloud.la/PMIXO%20Product%20Line/Office%20Furniture/Conference%20Tables.png?sign=6f1b596c5b6bedf7729cdced1499a864&t=1645431294'
+        },
+        {
+          label: 'Discussion Tables',
+          url: '',
+          imageURL: 'https://706d-pmixo1-6gno1a2f0b6f1c2d-1308466752.tcb.qcloud.la/PMIXO%20Product%20Line/Office%20Furniture/Discussion%20Tables.png?sign=9572e71000285b4cea4493fe748f314d&t=1645431306'
+        },
+        {
+          label: 'Filing Cabinets',
+          url: '',
+          imageURL: 'https://706d-pmixo1-6gno1a2f0b6f1c2d-1308466752.tcb.qcloud.la/PMIXO%20Product%20Line/Office%20Furniture/Filing%20Cabinets.png?sign=b5b455a4da538df092cc323f021695b6&t=1645431321'
+        },
+        {
+          label: 'Office Chairs',
+          url: '',
+          imageURL: 'https://706d-pmixo1-6gno1a2f0b6f1c2d-1308466752.tcb.qcloud.la/PMIXO%20Product%20Line/Office%20Furniture/Office%20Chairs.png?sign=1255b4968dc462a89f05fe442958be35&t=1645431332'
+        },
+        {
+          label: 'Office Desks',
+          url: '',
+          imageURL: 'https://706d-pmixo1-6gno1a2f0b6f1c2d-1308466752.tcb.qcloud.la/PMIXO%20Product%20Line/Office%20Furniture/Office%20Desks.png?sign=6d6bc76556820f2f689839cf1b71c117&t=1645431347'
+        },
+        {
+          label: 'Sofas',
+          url: '',
+          imageURL: 'https://706d-pmixo1-6gno1a2f0b6f1c2d-1308466752.tcb.qcloud.la/PMIXO%20Product%20Line/Office%20Furniture/Sofas.png?sign=b759334e014cac605b28989272a2498d&t=1645431362'
+        },
+        {
+          label: 'Tea Tables',
+          url: '',
+          imageURL: 'https://706d-pmixo1-6gno1a2f0b6f1c2d-1308466752.tcb.qcloud.la/PMIXO%20Product%20Line/Office%20Furniture/Tea%20Tables.png?sign=451ea7fe0f0eeebea3112f964aab636c&t=1645431373'
+        },
       ]
     },
     {
-      label: '无线智能周边',
-      id: '无线智能周边',
+      label: 'Wireless Accessories',
+      id: 'wireless-accessories',
       categories: [
         {
-          label: '书桌/书桌椅4',
+          label: 'Wireless Charging Humidifiers',
           url: '',
-          imageURL: 'https://res.app.ikea.cn/static/public/subcatalog/bm003/thumbnail.png?v=20220104'
-        }
-      ]
-    },
-    {
-      label: '全屋定制',
-      id: '全屋定制',
-      categories: [
+          imageURL: 'https://706d-pmixo1-6gno1a2f0b6f1c2d-1308466752.tcb.qcloud.la/PMIXO%20Product%20Line/Wireless%20Accessories/Wireless%20Charging%20Humidifiers.png?sign=a5d96c2ea22f598ef3d2cdabb7b30c85&t=1645431444'
+        },
         {
-          label: '书桌/书桌椅5',
+          label: 'Wireless Charging Keyboards',
           url: '',
-          imageURL: 'https://res.app.ikea.cn/static/public/subcatalog/bm003/thumbnail.png?v=20220104'
-        }
+          imageURL: 'https://706d-pmixo1-6gno1a2f0b6f1c2d-1308466752.tcb.qcloud.la/PMIXO%20Product%20Line/Wireless%20Accessories/Wireless%20Charging%20Keyboards.png?sign=579ab9adc043e5f16f7e0616a2c53ffb&t=1645431456'
+        },
+        {
+          label: 'Wireless Charging Laptop Stands',
+          url: '',
+          imageURL: 'https://706d-pmixo1-6gno1a2f0b6f1c2d-1308466752.tcb.qcloud.la/PMIXO%20Product%20Line/Wireless%20Accessories/Wireless%20Charging%20Laptop%20Stands.jpg?sign=5d9d570cc2a82a7fe697b2e4cd28cfdf&t=1645431470'
+        },
+        {
+          label: 'Wireless Charging Mouses',
+          url: '',
+          imageURL: 'https://706d-pmixo1-6gno1a2f0b6f1c2d-1308466752.tcb.qcloud.la/PMIXO%20Product%20Line/Wireless%20Accessories/Wireless%20Charging%20Mouses.png?sign=1430d49e7664e7931b5eb709ac386831&t=1645431489'
+        },
+        {
+          label: 'Wireless Charging Stereo Speakers',
+          url: '',
+          imageURL: 'https://706d-pmixo1-6gno1a2f0b6f1c2d-1308466752.tcb.qcloud.la/PMIXO%20Product%20Line/Wireless%20Accessories/Wireless%20Charging%20Stereo%20Speakers.png?sign=a9073c082ac7939dc8d40c4f846891db&t=1645431501'
+        },
+        {
+          label: 'Wireless Charging Table Lamp',
+          url: '',
+          imageURL: 'https://706d-pmixo1-6gno1a2f0b6f1c2d-1308466752.tcb.qcloud.la/PMIXO%20Product%20Line/Wireless%20Accessories/Wireless%20Charging%20Table%20Lamp.png?sign=a406fd488ab427a43e9b55b4a5a14dbe&t=1645431511'
+        },
       ]
     }
   ],
